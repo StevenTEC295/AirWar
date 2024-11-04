c1f3343e81956110ae084cc597927645e923c79f
StevenTEC295 <165940741+StevenTEC295@users.noreply.github.com>
2024-11-03 18:54:38 -0600

    Agregué el mapa y la cuenta regresiva

--- AirWar/MainWindow.xaml.cs
+++ AirWar/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AirWar
 {
@@ -20,9 +21,40 @@ namespace AirWar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        private TimeSpan time;
         public MainWindow()
         {
             InitializeComponent();
+            IniciarCuentaRegresiva();
+        }
+
+        private void IniciarCuentaRegresiva()
+        {
+            // Establece el tiempo inicial de la cuenta regresiva (ej. 5 minutos)
+            time = TimeSpan.FromMinutes(5);
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (time.TotalSeconds > 0)
+            {
+                time = time.Subtract(TimeSpan.FromSeconds(1));
+                TimerLabel.Content = time.ToString(@"mm\:ss");
+            }
+            else
+            {
+                timer.Stop();
+                TimerLabel.Content = "¡Tiempo terminado!";
+                // Puedes agregar aquí la lógica para finalizar el juego o mostrar un mensaje.
+            }
         }
     }
 }
